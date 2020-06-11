afdec82c7f815ba49ad082e1eb1ac5a636942eb3
Simeon Adegbola <adegbolasimeon@gmail.com>
2020-06-11 11:29:26 +0100

    Delete util.js

--- util.js
+++
@@ -1,8 +0,0 @@
-// utils and other stuff worth noting
-const sample_req = {
-    name: "Mark Essien",
-    email: "markessien@gmail.com",
-  }
-const sample_res = {
-    "message": "We sent a one use code to markessien@gmail.com"
-  }

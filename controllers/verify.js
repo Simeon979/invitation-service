fba43340b0daffd3a737489166866b66ad742216
Simeon Adegbola <adegbolasimeon@gmail.com>
2020-06-11 11:26:38 +0100

    fix merge conflict

--- controllers/verify.js
+++ controllers/verify.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let { body, sanitizeBody, validationResult } = require("express-validator");
 
 let { verify, remove } = require("../services/store");
@@ -38,6 +37,3 @@ const verifyHandler = [
 ];
 
 module.exports = verifyHandler;
-=======
-// verification logic goes here
->>>>>>> a5ce4fd... First commits

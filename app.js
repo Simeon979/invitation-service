9183b60fd7decfa0f49f960c0944848b448c3a96
Simeon Adegbola <adegbolasimeon@gmail.com>
2020-06-11 11:25:34 +0100

    fix merge conflict

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 9756929... Fixed all according to spec
 
 const sendHandler = require("./controllers/send");
 const verifyHandler = require("./controllers/verify");
 const getAllHandler = require("./controllers/getAll");
-<<<<<<< HEAD
-=======
-const inviteRoute = require('./routes/inviteRoute');
->>>>>>> a5ce4fd... First commits
-=======
->>>>>>> 9756929... Fixed all according to spec
+
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-<<<<<<< HEAD
-<<<<<<< HEAD
 app.get("/invites", getAllHandler);
 app.post("/invites/send", sendHandler);
 app.post("/invites/verify", verifyHandler);
@@ -30,14 +19,6 @@ app.use((req, res, next) => {
     message: "the requested resource was not found",
   });
 });
-=======
-
->>>>>>> a5ce4fd... First commits
-=======
-// app.get("/invites", getAllHandler);
-app.post("/invites/send", sendHandler);
-// app.post("/invites/verify", verifyHandler);
->>>>>>> 9756929... Fixed all according to spec
 
 app.use((err, req, res, next) =>
   res.status(500).json({
